0ab8af8fdcb1480de68bef01c6ddf0b998d04fc4
chedadsp <chedadsp@gmail.com>
2020-12-08 20:26:37 +0100

    Searching Points in a KD-Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -48,10 +48,31 @@ struct KdTree
 		insertRecursive(root, point, id, 0);
 	}
 
+	void searchRecursive(std::vector<float> target, float distanceTol, Node* node, int depth, std::vector<int>& ids)
+	{
+		if(node != NULL)
+		{
+			// TODO: make this capable for more then 2D
+			if( (node->point[0]>=(target[0] - distanceTol) && node->point[0] <= (target[0] + distanceTol)) && 
+				(node->point[1]>=(target[1] - distanceTol) && node->point[1] <= (target[1] + distanceTol)))
+				{
+					float distance = sqrt(pow(node->point[0] - target[0] ,2) + pow(node->point[1] - target[1] ,2));
+					if(distance <= distanceTol)
+						ids.push_back(node->id);
+				}
+			if((target[depth % target.size()] - distanceTol) < node->point[depth % target.size()])
+				searchRecursive(target, distanceTol, node->left, depth + 1, ids);
+			if((target[depth % target.size()] - distanceTol) < node->point[depth % target.size()])
+				searchRecursive(target, distanceTol, node->right, depth + 1, ids);
+		}
+	}
+
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchRecursive(target, distanceTol, root, 0, ids);
+
 		return ids;
 	}
 	
